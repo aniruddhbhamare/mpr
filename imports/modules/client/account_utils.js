52115177f91d8083cf85daad86330021515e605c
sushilshimpi <sushil.shimpi@gmail.com>
2019-02-17 22:49:45 -0700

    feat(avatar): added server and client side js to show avatar

--- imports/modules/client/account_utils.js
+++ imports/modules/client/account_utils.js
@@ -1,30 +1,49 @@
-import {Meteor} from "meteor/meteor";
+import { Meteor } from "meteor/meteor";
 
 export const userEmail = function() {
-	let user = Meteor.user();
-	if(!user) {
-		return "";
-	}
+  console.log(`acccount_utils|userEmail`);
+  let user = Meteor.user();
+  if (!user) {
+    return "";
+  }
 
-	if(user.emails && user.emails.length) {
-		return user.emails[0].address || "";
-	}
+  if (user.emails && user.emails.length) {
+    return user.emails[0].address || "";
+  }
 
-	if(user.private && user.private.email) {
-		return user.private.email;
-	}
+  if (user.private && user.private.email) {
+    return user.private.email;
+  }
 
-	return "";
+  return "";
 };
 
 export const userFullName = function() {
-	let user = Meteor.user();
-	if(!user || !user.profile) {
-		return "";
-	}
+  console.log(`acccount_utils|userFullName`);
+  let user = Meteor.user();
+  if (!user || !user.profile) {
+    return "";
+  }
 
-	if(user.profile.firstName || user.profile.lastName) {
-		return (user.profile.firstName || "") + (user.profile.lastName ? (" " + (user.profile.lastName || "")) : "");
-	}
-	return user.profile.name || "";
+  if (user.profile.firstName || user.profile.lastName) {
+    return (
+      (user.profile.firstName || "") +
+      (user.profile.lastName ? " " + (user.profile.lastName || "") : "")
+    );
+  }
+  return user.profile.name || "";
+};
+
+export const userAvatar = function() {
+  console.log(`acccount_utils|userAvatar`);
+  let user = Meteor.user();
+  if (!user) {
+    return "";
+  }
+
+  if (user.private && user.private.picture) {
+    return user.private.picture;
+  }
+
+  return "";
 };
